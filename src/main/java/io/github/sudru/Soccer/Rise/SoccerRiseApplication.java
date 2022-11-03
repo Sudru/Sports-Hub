345c37809a39ac7ac110a22376a40a697909c6fb
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-03 21:00:45 +0545

    first commit

---
+++ src/main/java/io/github/sudru/Soccer/Rise/SoccerRiseApplication.java
@@ -0,0 +1,13 @@
+package io.github.sudru.Soccer.Rise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerRiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerRiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sudru/Soccer/Rise/SoccerRiseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sudru.Soccer.Rise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerRiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
