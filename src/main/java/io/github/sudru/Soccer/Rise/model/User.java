e8a4ec23cc78b166102c4da7c915ed9e2ab0ece8
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-05 15:17:02 +0545

    user entity created

---
+++ src/main/java/io/github/sudru/Soccer/Rise/model/User.java
@@ -0,0 +1,23 @@
+package io.github.sudru.Soccer.Rise.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User  {
+    @Id
+    private ObjectId _id;
+    private String name;
+    private String email;
+    private String password;
+
+}
