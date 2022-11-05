33a04aab5818b3085d31273f9026215dca4758e7
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-05 15:26:05 +0545

    userdetails and userdetails service implemented

---
+++ src/main/java/io/github/sudru/Soccer/Rise/service/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package io.github.sudru.Soccer.Rise.service;
+
+import io.github.sudru.Soccer.Rise.model.User;
+import io.github.sudru.Soccer.Rise.model.UserDetailsImpl;
+import io.github.sudru.Soccer.Rise.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        return new UserDetailsImpl(user);
+    }
+}
