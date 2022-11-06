82f5798cb006f52d6a8592de164688a6c301dabb
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-06 11:17:44 +0545

    username not found exception added if user not found

--- src/main/java/io/github/sudru/Soccer/Rise/service/UserDetailsServiceImpl.java
+++ src/main/java/io/github/sudru/Soccer/Rise/service/UserDetailsServiceImpl.java
@@ -15,7 +15,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username);
+        User user = userRepository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("Invalid Credentials"));
         return new UserDetailsImpl(user);
     }
 }
