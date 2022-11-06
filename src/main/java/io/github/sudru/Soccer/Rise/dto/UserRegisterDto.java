54ccfff849f89e187a3d77baa3db5f54d4df2d06
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-06 11:16:20 +0545

    validation added in register dto

--- src/main/java/io/github/sudru/Soccer/Rise/dto/UserRegisterDto.java
+++ src/main/java/io/github/sudru/Soccer/Rise/dto/UserRegisterDto.java
@@ -2,13 +2,19 @@ package io.github.sudru.Soccer.Rise.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.io.Serializable;
 
 @Data
 
 public class UserRegisterDto  implements Serializable {
+    @NotBlank(message = "Field Cannot be empty")
     private String name;
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Invalid email")
     private String email;
+    @Size(min = 8 ,message = "Password must be at least 8 characters long")
     private String password;
+    @Min(8)
     private String confirmPassword;
 }
