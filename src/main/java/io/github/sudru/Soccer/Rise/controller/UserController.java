5890a78059abd30b07ff6eb4d1bc0d285c3f59eb
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-06 11:13:33 +0545

    register errors added in the template

--- src/main/java/io/github/sudru/Soccer/Rise/controller/UserController.java
+++ src/main/java/io/github/sudru/Soccer/Rise/controller/UserController.java
@@ -5,9 +5,14 @@ import io.github.sudru.Soccer.Rise.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @Slf4j
@@ -16,15 +21,28 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        model.addAttribute(new UserRegisterDto());
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerUser(@ModelAttribute UserRegisterDto dto) {
-        log.info(dto.toString());
-        userService.registerUser(dto);
-        return "redirect:/register";
+    public String registerUser(@Valid @ModelAttribute UserRegisterDto userRegisterDto, BindingResult result, Model model) {
+        log.info(userRegisterDto.toString());
+        if (result.getErrorCount() > 0) {
+            if (!userRegisterDto.getPassword().equals(userRegisterDto.getConfirmPassword())) {
+                model.addAttribute("pswe", "Password do not match");
+            }
+            return "/register";
+        }
+
+        if (userService.registerUser(userRegisterDto))
+            model.addAttribute("success", "User Added Successfully.");
+        else
+            model.addAttribute("success","User Already Exists.");
+        return "/register";
+
+
     }
 
 }
