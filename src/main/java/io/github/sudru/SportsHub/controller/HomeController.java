14059c48cc51039827b4cf59beaebf05dffa36c2
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-08 13:57:09 +0545

    home controller set to return the home page

--- src/main/java/io/github/sudru/SportsHub/controller/HomeController.java
+++ src/main/java/io/github/sudru/SportsHub/controller/HomeController.java
@@ -1,14 +1,11 @@
 package io.github.sudru.SportsHub.controller;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping
-    public ResponseEntity<?> home(){
-        return new ResponseEntity<>("Welcome to SoccerRise",HttpStatus.OK);
+    public String home(){
+        return "home";
     }
 }
