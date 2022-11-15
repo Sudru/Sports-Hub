d6839ff0d91f2329d3a570efc491c31fa8fcd67f
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-15 13:36:29 +0545

    tournament controller for new tournament and form for registration

--- src/main/java/io/github/sudru/SportsHub/controller/TournamentController.java
+++ src/main/java/io/github/sudru/SportsHub/controller/TournamentController.java
@@ -1,18 +1,31 @@
 package io.github.sudru.SportsHub.controller;
 
+import io.github.sudru.SportsHub.dto.SportType;
+import io.github.sudru.SportsHub.dto.TournamentRegisterDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Slf4j
 public class TournamentController {
 
     @GetMapping("/tournaments")
-    public String tournaments(){
+    public String tournaments(Model model){
+        model.addAttribute("sportsTypes",SportType.values());
+        model.addAttribute(new TournamentRegisterDto());
         return "tournaments";
     }
     @GetMapping("/tournament/{id}")
     public String details(@PathVariable String id){
         return "tournament-details";
+    }
+    @PostMapping("/tournaments")
+    public String createTournament(@ModelAttribute TournamentRegisterDto dto){
+        log.info(dto.toString());
+
+        return "redirect:/tournaments";
+
     }
 }
