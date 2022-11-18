9375d488bfcb94705ad193e58c3542b7dac0b999
Sudarshan <sudarshandevkota22@gmail.com>
2022-11-18 15:31:26 +0545

    team controller to add new team to a tournament and database

--- src/main/java/io/github/sudru/SportsHub/controller/TeamController.java
+++ src/main/java/io/github/sudru/SportsHub/controller/TeamController.java
@@ -1,6 +1,8 @@
 package io.github.sudru.SportsHub.controller;
 
 import io.github.sudru.SportsHub.dto.TeamDto;
+import io.github.sudru.SportsHub.service.TeamService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
+@RequiredArgsConstructor
 public class TeamController {
+    private final TeamService teamService;
+
     @PostMapping("team/{id}")
     public String registerTeam(@PathVariable String id, @ModelAttribute TeamDto dto){
         log.info(dto.toString());
+        teamService.registerTeam(id,dto);
         return "redirect:/";
     }
 }
